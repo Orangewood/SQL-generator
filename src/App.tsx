20e3ab5b32b21b5a5e912c2ed3a89acab10f7598
orangewood <jamesdolan1989@gmail.com>
2021-07-23 19:26:03 -0400

    Clear warnings

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import Body from "./components/Body";
 import SqlBox from "./components/SqlBox";
 import { RowDictionary } from "./services/AppTypes";
